c4de9152f34d2831d43dfa3769a7a6452bba5814
Matt Stibbs <git@stibbsy.co.uk>
2015-01-14 22:38:21 +0000

    Update method to check both username and password

--- blockbuster/bb_security.py
+++ blockbuster/bb_security.py
@@ -6,6 +6,6 @@ from blockbuster import bb_dbconnector_factory
 def credentials_are_valid(username, password):
     db = bb_dbconnector_factory.DBConnectorInterfaceFactory().create()
     print(username)
-    result = db.api_username_exists(username)
+    result = db.api_credentials_are_valid(username, password)
     print (result)
     return result
