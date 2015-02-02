d273f698425f20ad113e6137869fa4545e926004
Matt Stibbs <git@stibbsy.co.uk>
2015-02-02 23:35:21 +0000

    Update bash scripts to reflect renamed celery module

--- provision/start_workers.sh
+++ provision/start_workers.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-/usr/bin/tmux new-session -s bb-workers -d 'cd /vagrant && export C_FORCE_ROOT="true" && /home/vagrant/env/bin/celery -A blockbuster_celery.BBCelery worker -l info -n worker10 -c 1' \; split-window -h -d 'cd /vagrant && export C_FORCE_ROOT="true" && sleep 5 && /home/vagrant/env/bin/celery -A blockbuster_celery.BBCelery worker -l info -n worker11 -c 1' \; attach
+/usr/bin/tmux new-session -s bb-workers -d 'cd /vagrant && export C_FORCE_ROOT="true" && /home/vagrant/env/bin/celery -A
+blockbuster_celery.bb_celery worker -l info -n worker10 -c 1' \; split-window -h -d 'cd /vagrant && export
+C_FORCE_ROOT="true" && sleep 5 && /home/vagrant/env/bin/celery -A blockbuster_celery.bb_celery worker -l info -n worker11 -c
+1' \; attach
