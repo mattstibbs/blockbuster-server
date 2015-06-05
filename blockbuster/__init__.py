9793dde3a5efac4c2a9760485685cdd71fdf3c7e
Matt Stibbs <git@stibbsy.co.uk>
2015-06-05 20:03:53 +0100

    Move section from run.py to __init__

--- blockbuster/__init__.py
+++ blockbuster/__init__.py
@@ -7,9 +7,27 @@ app = Flask(__name__)
 
 
 def startup():
+    import datetime
     import blockbuster.bb_dbconnector_factory
     import blockbuster.bb_logging as log
     import blockbuster.bb_auditlogger as audit
+
+    blockbuster.app.debug = blockbuster.config.debug_mode
+
+    blockbuster.bb_logging.logger.debug("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    blockbuster.bb_logging.logger.debug("@@@@@@@@@@@@@@@@@@ BlockBuster " + blockbuster.__version__ + " "
+                                                                                                            "@@@@@@@@@@@@@@@@@@")
+    blockbuster.bb_logging.logger.debug("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    blockbuster.bb_logging.logger.info("Application Startup")
+    blockbuster.bb_logging.logger.info(
+        'Application Setting - Time Restriction Disabled') \
+        if not blockbuster.config.timerestriction else blockbuster.bb_logging.logger.info(
+        'Application Setting - Time Restriction Enabled')
+
+    # blockbuster.bb_logging.logger.debug("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+
+    if blockbuster.config.debug_mode:
+        blockbuster.bb_logging.logger.info("========= APPLICATION IS RUNNING IN DEBUG MODE ==========")
 
     try:
         if blockbuster.bb_dbconnector_factory.DBConnectorInterfaceFactory().create().db_version_check():
