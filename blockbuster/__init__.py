34ac121ff9ddfcfd19983980107238a6d66a3464
Matt Stibbs <git@stibbsy.co.uk>
2015-06-05 20:14:05 +0100

    Remove crud

--- blockbuster/__init__.py
+++ blockbuster/__init__.py
@@ -7,24 +7,18 @@ app = Flask(__name__)
 
 
 def startup():
-    import datetime
     import blockbuster.bb_dbconnector_factory
     import blockbuster.bb_logging as log
     import blockbuster.bb_auditlogger as audit
 
     blockbuster.app.debug = blockbuster.config.debug_mode
 
-    blockbuster.bb_logging.logger.debug("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-    blockbuster.bb_logging.logger.debug("@@@@@@@@@@@@@@@@@@ BlockBuster " + blockbuster.__version__ + " "
-                                                                                                            "@@@@@@@@@@@@@@@@@@")
-    blockbuster.bb_logging.logger.debug("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
-    blockbuster.bb_logging.logger.info("Application Startup")
+    blockbuster.bb_logging.logger.info(str.format("Application Startup - BlockBuster v{0} Schema v{1}",
+                                                  blockbuster.__version__, target_schema_version))
     blockbuster.bb_logging.logger.info(
         'Application Setting - Time Restriction Disabled') \
         if not blockbuster.config.timerestriction else blockbuster.bb_logging.logger.info(
         'Application Setting - Time Restriction Enabled')
-
-    # blockbuster.bb_logging.logger.debug("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
 
     if blockbuster.config.debug_mode:
         blockbuster.bb_logging.logger.info("========= APPLICATION IS RUNNING IN DEBUG MODE ==========")
@@ -35,10 +29,10 @@ def startup():
             print("Running...")
 
         else:
-            raise RuntimeError("Incorrect database schema version.")
+            raise RuntimeError("Incorrect database schema version. Wanted ")
 
     except RuntimeError, e:
-        log.logger.exception("Incorrect database schema version.")
-        audit.BBAuditLoggerFactory().create().logException('app', 'STARTUP', 'Incorrect database schema version.')
+        log.logger.exception(e)
+        audit.BBAuditLoggerFactory().create().logException('app', 'STARTUP', str(e))
 
 startup()
