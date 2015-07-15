9997423c87ffaed860afd04ddc5e8298d00b6935
Matt Stibbs <matthew.stibbs@advancedcomputersoftware.com>
2015-07-15 13:23:49 +0100

    Some changes (to be reviewed)

--- provision/exec/start_app.sh
+++ provision/exec/start_app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd ~/production/blockbuster-server/
-/root/production/blockbuster-server/env/bin/gunicorn blockbuster:app -w 2 -b localhost:8000 --reload
+cd /opt/blockbuster/blockbuster-server/
+/opt/blockbuster/env/bin/gunicorn blockbuster:app -w 2 -b localhost:8000 --reload
