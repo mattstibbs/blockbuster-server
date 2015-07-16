6413625394c0fa9b2743c459368e87e52a3f01ff
Matt Stibbs <git@stibbsy.co.uk>
2015-07-16 23:27:11 +0100

    Improve the Vagrant image so that it is closer to a production setup. Add support for blockbuster as upstart services.

--- provision/bootstrap-config.sh
+++ provision/bootstrap-config.sh
@@ -2,13 +2,13 @@
 # Bootstrap script to seed the project with config files
 
 echo "Seeding project with config files if they don't already exist..."
-cd /vagrant/blockbuster
+cd /opt/blockbuster/blockbuster-server
 
 if [ -e "config.py" ]
 then
   echo "NO CHANGE - config.py file already present"
 else
-	cp /vagrant/blockbuster/example_config_files/example_config.py /vagrant/blockbuster/config.py
+	cp /opt/blockbuster/blockbuster-server/blockbuster/example_config_files/example_config.py /opt/blockbuster/blockbuster-server/blockbuster/config.py
   	echo "Seeding project using example config.py file"
 fi
 
@@ -16,7 +16,7 @@ if [ -e "config_services.py" ]
 then
   echo "NO CHANGE - config_services.py file already present"
 else
-  cp /vagrant/blockbuster/example_config_files/example_config_services.py /vagrant/blockbuster/config_services.py
+  cp /opt/blockbuster/blockbuster-server/blockbuster/example_config_files/example_config_services.py /opt/blockbuster/blockbuster-server/blockbuster/config_services.py
   echo "Seeding project using example config_services.py file"
 fi
 
@@ -24,6 +24,6 @@ if [ -e "config_celery.py" ]
 then
   echo "NO CHANGE - config_celery.py file already present"
 else
-  cp /vagrant/blockbuster/example_config_files/example_config_celery.py /vagrant/blockbuster/config_celery.py
+  cp /opt/blockbuster/blockbuster-server/example_config_files/example_config_celery.py /opt/blockbuster/blockbuster-server/blockbuster/config_celery.py
   echo "Seeding project using example config_celery.py file"
 fi
