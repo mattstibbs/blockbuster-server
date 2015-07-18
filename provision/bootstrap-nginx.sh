67a091cf3385472e79f0981bfa7e24896543fce9
Matt Stibbs <git@stibbsy.co.uk>
2015-07-18 17:30:50 +0100

    Update several files to make vagrant image work properly.

--- provision/bootstrap-nginx.sh
+++ provision/bootstrap-nginx.sh
@@ -11,7 +11,7 @@ if [ -e "blockbuster-server" ]
 then
   echo "NO CHANGE - blockbuster-server site file already present"
 else
-	cp /opt/blockbuster/blockbuster-server/provision/nginx/blockbuster-server /etc/nginx/sites-available/
+	cp /opt/blockbuster/blockbuster-server/provision/nginx/blockbuster-server /etc/nginx/sites-available/blockbuster-server
   	echo "Copying blockbuster-server site config into sites-available"
   	ln -s /etc/nginx/sites-available/blockbuster-server /etc/nginx/sites-enabled/blockbuster-server
 fi
