ad8d350c8616233e41b5188d9d216af2e529e5cb
Matt Stibbs <git@stibbsy.co.uk>
2015-07-20 08:52:39 +0100

    Update start_all bash script

--- provision/start_all.sh
+++ provision/start_all.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 /usr/bin/tmux new-session -s blockbuster-server -d 'cd /opt/blockbuster/blockbuster-server &&
-/opt/blockbuster/env/bin/gunicorn
-blockbuster:app -b
-localhost:8000 --reload' \; split-window -d 'cd /opt/blockbuster/blockbuster-server && export C_FORCE_ROOT="true" &&
-/opt/blockbuster/env/bin/celery -A
-blockbuster_celery.bb_celery worker -c 1 -l info -n worker1' \; attach
+/opt/blockbuster/env/bin/gunicorn blockbuster:app -b localhost:8000 --reload' \; split-window -d 'cd
+/opt/blockbuster/blockbuster-server && export C_FORCE_ROOT="true" && /opt/blockbuster/env/bin/celery -A blockbuster_celery
+.bb_celery worker -c 1 -l info -n worker1' \; attach
