fcb26bfc37cafc84d61f9f9c2f2e5f1ad986758f
Matt Stibbs <git@stibbsy.co.uk>
2017-10-03 18:28:43 +0100

    Update message handlers to make use of RQ instead of Celery

--- blockbuster/messaging/bb_sms_handler.py
+++ blockbuster/messaging/bb_sms_handler.py
@@ -1,17 +1,25 @@
 # This is the entry point for sending SMS via the background queue
 
 # Local imports
+import blockbuster.config as config
 import logging
+import redis
+from rq import Queue
 
-from blockbuster_celery.bb_celery import bg_worker
+# Set up RQ queue
+conn = redis.from_url(config.REDIS_URL)
+q = Queue(connection=conn)
+
 import blockbuster.bb_auditlogger
 from blockbuster.messaging import bb_sms_sender
 
 logger = logging.getLogger(__name__)
 
+
 def send_sms_notification(originator, recipient, body):
     try:
-        send_sms.delay(originator, recipient, body)
+        # send_sms.delay(originator, recipient, body)
+        q.enqueue(send_sms, originator, recipient, body)
         logger.debug("SMS notification queued.")
 
     except Exception as e:
@@ -21,7 +29,6 @@ def send_sms_notification(originator, recipient, body):
         blockbuster.bb_auditlogger.BBAuditLoggerFactory().create().logException('app', 'QUEUE-SMS', str(e))
 
 
-@bg_worker.task
 def send_sms(originator, recipient, body):
     logger.debug("Starting SMS send")
     bb_sms_sender.SMSSenderFactory().create().send_sms(originator, recipient, body)
