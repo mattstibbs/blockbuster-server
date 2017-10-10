0ba471488859d3184eca0384fae1b9013dece73a
Matt Stibbs <git@stibbsy.co.uk>
2017-10-10 22:58:17 +0100

    Update heroku worker using the recommended RQ HerokuWorker approach

--- blockbuster/heroku_worker.py
+++ blockbuster/heroku_worker.py
@@ -1,15 +1,20 @@
-import redis
-from rq import Worker, Queue, Connection
 import os
+from redis import Redis
+from rq import Queue, Connection
+from rq.worker import HerokuWorker as Worker
+
+
+listen = ['high', 'default', 'low']
 
 REDIS_URL = os.environ.get('REDIS_URL', 'redis://localhost:32769/1')
-print(REDIS_URL)
 
-listen = ['default']
+if not REDIS_URL:
+    raise RuntimeError('Set up Redis first.')
 
-conn = redis.from_url(REDIS_URL)
+conn = Redis.from_url(REDIS_URL)
 
 if __name__ == '__main__':
     with Connection(conn):
         worker = Worker(map(Queue, listen))
         worker.work()
+
